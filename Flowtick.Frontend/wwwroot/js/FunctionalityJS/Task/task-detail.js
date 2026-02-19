b10bb1870b06d09e696cd5f33f51bebf4127a1e8
Shahid Ullah <shahid@bha-accountancy.com>
2026-02-19 15:39:18 +0500

    Task Detail Time To Local

--- Flowtick.Frontend/wwwroot/js/FunctionalityJS/Task/task-detail.js
+++ Flowtick.Frontend/wwwroot/js/FunctionalityJS/Task/task-detail.js
@@ -751,8 +751,7 @@
 		 });
 	 }
 	 var getCommentsCallBack = function (response) {
-		 if (response.request.status === 200) {
-			 console.log(response.data);
+		 if (response.request.status === 200) {			 
 			 const getComments = response.data;
 			 renderComments(getComments);
 
@@ -769,7 +768,12 @@
 		 $.each(comments, function (index, comment) {
 			 const isLoginUser = comment.user.id === Number(loginUserID);
 			 const sideClass = isLoginUser ? "right" : "left";
-			 const userName = comment.user.name
+             const userName = comment.user.name
+
+             const utcDateTime = new Date(`${comment.date}T${comment.time}:00Z`);
+             //comment.date  = utcDateTime.toLocaleDateString(); 2/2/2026
+             comment.time = utcDateTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });    
+
 			 const html = `
 					 <div class="comment-item ${sideClass}" data-id="${comment.id}">
 
