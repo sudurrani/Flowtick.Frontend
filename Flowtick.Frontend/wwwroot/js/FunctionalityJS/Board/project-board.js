7c73120eb425e3f2372c95d75741ea52b5f05bb7
naumankhan8bha <nauman.khan.bha@outlook.com>
2026-05-14 18:19:31 +0500

    Add New Desing For Add Member, Add Board, Create Project, Create Sprint

--- Flowtick.Frontend/wwwroot/js/FunctionalityJS/Board/project-board.js
+++ Flowtick.Frontend/wwwroot/js/FunctionalityJS/Board/project-board.js
@@ -382,6 +382,14 @@ $(function () {
         });
     });
 
+    $("#ft-add-project").click(function () {
+        $('#ft-overlay-project').removeClass('ft-hidden').addClass('ft-overlay--visible');
+
+        $('.ft-dropdown-panel--right').addClass('ft-dropdown--hidden');
+        $('#ft-add-btn').attr('aria-expanded', 'false');
+    })
+
+
     if (!isTaskStatusesLoaded) {
         getTaskStatus();
     }
